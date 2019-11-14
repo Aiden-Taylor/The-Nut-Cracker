aec0c4ab19062099267578bcc59b96ac60be03b6
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-13 21:19:04 -0800

    Update Ubu14Script.py

--- Ubu14Script.py
+++ Ubu14Script.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#to make executable, run the command: chmod +x Ubu14Script.py
+#to run, type: sudo python3 ./Ubu14Script.py
 import subprocess as yeet
 print("Welcome to Aiden's Nut Cracker, better check your cup")
 print("Also, pay homage to Aiden, he is about to get you fat points")
@@ -7,26 +10,28 @@ users = input("Does a user need to be removed, fart sniffer? (y/n)")
 if users == "y" or users == "Y":
     bump = 0
 while bump == 0:
-    remove = input("Name of the user, idiot?")
-    yeet.run(["userdel ", remove], shell=True)
+    yeet.run(["userdel", input("Name of the user, idiot?")], shell=False)
     users = input("Does another user need to be removed, booger connoisseur? (y/n)")
     if users != "y" and users != "Y":
         bump = 1
-print("Installing bum")
-yeet.run(["apt-get install bum"], shell=True)
-‘’’print("Installing and enabling UFW")
-yeet.run(["apt-get ufw"], shell=True)
-yeet.run(["ufw enable"], shell=True)
+if input("Install bum, forehead? (y/n)") == ("y" or "Y"):
+    print("Installing bum")
+    yeet.run(["apt-get install bum"], shell=False)
+if input("Set up UFW, brick? (y/n)") == ("y" or "Y"):
+    print("Installing and enabling UFW")
+    yeet.run(["apt-get ufw"], shell=False)
+    yeet.run(["ufw enable"], shell=False)
 #admin removal
-admins = input("Does anyone need to be removed from admin permissions, dummy? (y/n)")
-if admins == "y" or admins = "Y":
+if input("Does anyone need to be removed from admin permissions, dummy? (y/n)") == ("y" or "Y"):
     bump = 0
 while bump == 0:
-    remove = input("Name of the user, idiot?")
-    yeet.run(["deluser ", remove, " admins"], shell=True)
+    yeet.run(["deluser", input("Name of the user, idiot?"), "sudo"], shell=False)
     users = input("Does anyone else need to be removed from admin permissions, Ivan? (y/n)")
     if users != "y" and users != "Y":
         bump = 1
+
+#TODO
+'''
 #replacing files for pwd policies and ect
 yeet.run(["cp -f [original file] [new file]"], shell=True) #change pass age/complexity in replacement file(s)
 #automatic update stuffs
@@ -38,4 +43,11 @@ yeet.run(["cp -f /etc/ssh/sshd_config [new file]"], shell=True) #change PermitRo
 print("installing upgrades and updates, fatty. Oh, I mean Cooper")
 yeet.run(["apt-get upgrade"], shell=True)
 yeet.run("apt-get update")
-‘’’
+#***application removal
+#write all packages to file
+#use sendmail to email file to off-image computer
+#https://tecadmin.net/ways-to-send-email-from-linux-command-line/
+#query for package purge
+#***game removal
+#splice the one-shot ubu removal code
+'''
