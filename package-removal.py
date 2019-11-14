61d48e1d6cc1d0f24a7207f076780d37f6aacb7d
NigerianPrinces <57740526+NigerianPrinces@users.noreply.github.com>
2019-11-14 08:19:59 -0800

    Update package-removal.py

--- package-removal.py
+++ package-removal.py
@@ -2,3 +2,17 @@
 #to make executable, run the command: chmod +x Ubu14Script.py
 #to run, type: sudo python3 ./Ubu14Script.py
 import subprocess as yeet
+bump = 1
+
+
+#package-removal
+users = input("What  (y/n)")
+if users == "y" or users == "Y":
+    bump = 0
+ 
+yeet.run([" cat > yeeters.txt"], shell=True)
+yeet.run(["apt list --installed | less >> yeeters.txt"], shell=True)
+yeet.run(["sendmail joshua.reddy@rocklinusd.org  < yeeters.txt"], shell=True)
+
+while bump == 0:
+    yeet.run(["apt-get purge", input("Frickin Name of the package, bill?")], shell=False)
