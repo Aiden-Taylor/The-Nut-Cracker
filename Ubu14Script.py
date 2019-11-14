565565318305aa8de1623bfd969d88dc5983b03e
JacksonElsemore <54335566+JacksonElsemore@users.noreply.github.com>
2019-11-14 13:50:46 -0800

    Update Ubu14Script.py

--- Ubu14Script.py
+++ Ubu14Script.py
@@ -3,6 +3,7 @@
 #to run, type: sudo python3 ./Ubu14Script.py
 
 import subprocess as yeet
+import threading as marxismRules
 print("Welcome to Aiden's Nut Cracker, better check your cup")
 print("Also, pay homage to Aiden, he is about to get you fat points")
 bump = 1
@@ -10,48 +11,64 @@ bump = 1
 #Logs
 yeet.run([" cat > log.txt"], shell=True)
 
-
+def concurrent_command(command_stuff) {
+	yeet.run([command_stuff],shell=True)
+}
 
 #upgrade time
 if input("Upgrade, Quakebuttock? (y/n)") == ("y" or "Y"):
+    threads = []
     print("installing upgrades and updates, fatty. Oh, I mean Cooper")
     yeet.run(["apt-get upgrade"], shell=True)
+    bees = marxismRules.Thread(target=concurrent_command,args=("apt-get upgrade"))
+    bees.start()
+    threads.append(bees)
     yeet.run(["apt-get update"], shell=True)
-    
+    bees = marxismRules.Thread(target=concurrent_command,args=("apt-get update"))
+    bees.start()
+    threads.append(bees)
+    for thread in threads:
+        thread.join()
     yeet.run(['echo "Updates Installed Hopefully" >> log.txt'], shell=True)
     
 #Ports
-things = ["69","6660","6661","6662","6663","6664","6665","6666","6667","6668","6669","23","135","411","412","61","21","20","3389","25","139"]
-kekel = input("Specifically block ports or just let the program go through its list ya weed smokin, cop shootin, bike stealin, watermelon munchin, finger lickin, white woman rapin, go for nothin respectable white person? (Y/N)")
-if kekel == "Y":
-	kekel = input("put in ports you want to block in format of     10,23,22,80... its pretty sad i need to explicitly state this, really goes to show the amount of brain cells you have")
-	things = kekel.split(",")
-for port in things:
-	yeet.run(["iptables -A INPUT -p tcp --destination-port",port,"-j DROP"],shell=false)
-	yeet.run(["iptables -A OUTPUT -p tcp --destination-port",port,"-j DROP"],shell=false)
-	print("Port #" + port + " has been blocked :)")
-kekel = input("Do you want to save changes across reboot? (Y/N)")
-if kekel == "Y":
-	yeet.run(["service iptables save"],shell=false)
+kekel = input("Do you wan some ports?")
+if kekel == "y" or kekel =="Y":
+	things = ["69","6660","6661","6662","6663","6664","6665","6666","6667","6668","6669","23","135","411","412","61","21","20","3389","25","139"]
+	kekel = input("Specifically block ports or just let the program go through its list ya weed smokin, cop shootin, bike stealin, watermelon munchin, finger lickin, white woman rapin, go for nothin respectable white person? (Y/N)")
+	if kekel == "Y":
+		kekel = input("put in ports you want to block in format of     10,23,22,80... its pretty sad i need to explicitly state this, really goes to show the amount of brain cells you have")
+		things = kekel.split(",")
+	for port in things: 
+		yeet.run(["ufw deny",port],shell=False)
+		print("Port #" + port + " has been blocked :)")
+		yeet.run(['echo "port #' + port + ' is done heker" >> log.txt'],shell=False)
+	kekel = input("Do you want to save changes across reboot? (Y/N)")
+	if kekel == "Y":
+		yeet.run(["service iptables save"],shell=False)
+	yeet.run(['echo "ports are heked you heker" >> log.txt'],shell=False)
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #                                        MOVING ON TO SERVICES
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
-things = ["telnet","vsftpd","apache2"]
-kekel = input("Specifically block services or just let the program go through its list ya weed smokin, cop shootin, bike stealin, watermelon munchin, finger lickin, white woman rapin, go for nothin respectable white person? (Y/N)")
-if kekel == "Y":
-	kekel = input("put in services you want to block in format of     telnet,ftp,service_name...")
-	print("PS ur mom is a ho")
-	things = kekel.split(",")
-for service in things:
-	yeet.run(["systemctl stop",service],shell=false)
-	yeet.run(["systemctl disable",service],shell=false)
-    yeet.run(["systemctl daemon-reload"],shell=false)
-    yeet.run(["systemctl reset-failed"],shell=false)
-	print("Service " + service + " has been blocked :)")
-
-    
-    
-    
+kekel = input("u wan some services mr dummy head?")
+if kekel == "y" or kekel == "Y":
+	things = ["telnet","vsftpd","apache2"]
+	kekel = input("Specifically block services or just let the program go through its list ya weed smokin, cop shootin, bike stealin, watermelon munchin, finger lickin, white woman rapin, go for nothin respectable white person? (Y/N)")
+	if kekel == "Y":
+		kekel = input("put in services you want to block in format of     telnet,ftp,service_name...")
+		print("PS ur mom is a ho")
+		things = kekel.split(",")
+	for service in things:
+		yeet.run(["systemctl stop",service],shell=False)
+		yeet.run(["systemctl disable",service],shell=False)
+		yeet.run(["systemctl daemon-reload"],shell=False)
+		yeet.run(["systemctl reset-failed"],shell=False)
+		print("Service " + service + " has been blocked :)")
+		yeet.run(['echo "Service '+service+' is blocked" >> log.txt'],shell=False)
+	yeet.run(['echo "Services done just like ur mom" >> log.txt'],shell=False)
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
+#                                        MOVING ON TO USERS
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #user removal
 users = input("Does a user need to be removed, fart sniffer? (y/n)")
 if users == "y" or users == "Y":
@@ -63,7 +80,9 @@ while bump == 0:
     users = input("Does another user need to be removed, booger connoisseur? (y/n)")
     if users != "y" and users != "Y":
         bump = 1
-
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
+#                                        MOVING ON TO PACKAGES
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #package-removal
 users = input("What  (y/n)")
 if users == "y" or users == "Y":
@@ -92,6 +111,9 @@ if input("Set up UFW, brick? (y/n)") == ("y" or "Y"):
     yeet.run(["ufw enable"], shell=False)
 
 #admin removal
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
+#                                        MOVING ON TO ADMINS
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 if input("Does anyone need to be removed from admin permissions, dummy? (y/n)") == ("y" or "Y"):
     bump = 0
 while bump == 0:
@@ -99,14 +121,16 @@ while bump == 0:
     users = input("Does anyone else need to be removed from admin permissions, Ivan? (y/n)")
     if users != "y" and users != "Y":
         bump = 1       
-
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
+#                                        MOVING ON TO GAME REMOVAL
+#---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #***game removal
 joe = input("Ok epic gamer, do you want me to delete gnome-games-common, aisleriot, sudoku, mahjongg, ace-of-penguins, gnomine, and gbrainy? (y/n)")
-if joe = "y" or joe = "Y":
+if joe == "y" or joe == "Y":
   yeet.run(["sudo apt-get purge gnome-games-common gbrainy && sudo apt-get autoremove"], shell=True)
   yeet.run(["sudo apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy"], shell=True)
 joe = input("Any uncommon games to remove, my zooma? (y/n)")
-if joe = "y" or joe = "Y":
+if joe == "y" or joe == "Y":
   yeet.run(["sudo apt-get purge", input("What's the game package name, ear banger?")], shell=False)
 
 #TODO
