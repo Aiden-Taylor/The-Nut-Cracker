43b5a00563326bcc3b10220bff1b54d162b13ad7
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-14 08:23:56 -0800

    Update Ubu14Script.py

--- Ubu14Script.py
+++ Ubu14Script.py
@@ -49,6 +49,12 @@ yeet.run("apt-get update")
 #use sendmail to email file to off-image computer
 #https://tecadmin.net/ways-to-send-email-from-linux-command-line/
 #query for package purge
-#***game removal
-#splice the one-shot ubu removal code
 '''
+#***game removal
+joe = input("Ok epic gamer, do you want me to delete gnome-games-common, aisleriot, sudoku, mahjongg, ace-of-penguins, gnomine, and gbrainy? (y/n)")
+if joe = "y" or joe = "Y":
+  yeet.run(["sudo apt-get purge gnome-games-common gbrainy && sudo apt-get autoremove"], shell=True)
+  yeet.run(["sudo apt-get purge aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy"], shell=True)
+joe = input("Any uncommon games to remove, my zooma? (y/n)")
+if joe = "y" or joe = "Y":
+  yeet.run(["sudo apt-get purge", input("What's the game package name, ear banger?")], shell=False)
