2dc21908f792ceb9632969587a3ba052ef3c1c1e
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-14 20:30:36 -0800

    Create PasswdFuckme.py

---
+++ PasswdFuckme.py
@@ -0,0 +1,5 @@
+import subprocess as yeet
+if input('Change all passwords to "Your%Mom%Is%Fucking%Gay69&420? (y/n)"' == ("y" or "Y"):
+	 yeet.run(["cut -d: -f1 /etc/passwd | wc -l"], shell=True)
+	 for i in input("What number did the shell just display, you glorified TI-84?"):
+	 	
