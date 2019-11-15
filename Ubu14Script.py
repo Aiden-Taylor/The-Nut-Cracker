9584791cdd4a5ef37b569cad307c87f4d6bb553a
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 14:35:46 -0800

    Update Ubu14Script.py
    im dumb

--- Ubu14Script.py
+++ Ubu14Script.py
@@ -119,10 +119,10 @@ while bump == 0:
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #                                        MOVING ON TO PASSWORDS
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
-if input('Change all passwords to "Your%Mom%Is%Fucking%Gay69&420? (y/n)"' == ("y" or "Y"):
+'''if input('Change all passwords to "Your%Mom&Is&Fucking&Gay69&420? (y/n)"' == ("y" or "Y"):
 	 yeet.run(["cut -d: -f1 /etc/passwd | wc -l"], shell=True)
 	 for i in input("What number did the shell just display, you glorified TI-84?"):
-	 	
+'''	 	
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #                                        MOVING ON TO PACKAGES
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
