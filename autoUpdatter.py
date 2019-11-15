e2104abcb7782da674d4d4e48203c3e2d37c5fa7
JacksonElsemore <54335566+JacksonElsemore@users.noreply.github.com>
2019-11-15 14:32:33 -0800

    Create autoUpdatter.py
    forgot to commit last time :(
    PS: How are planes like people?
    When they catch on fire they stop, drop and roll!

---
+++ autoUpdatter.py
@@ -0,0 +1,7 @@
+import subprocess as yeet
+kekel = input("You wan to do the upating thing you knuckle headed bufoon")
+if kekel == "Y" or kekel == "y":
+  yeet.run(["apt-get install unattended-upgrades"],shell=False)
+  yeet.run(["sed -i 's/fuck/Jackson/g' /etc/apt/apt.conf.d/10periodic"],shell=True) #change fuck and Jackson into what you want
+ 
+  
