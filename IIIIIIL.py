8216ac4b72dc535a0b52da8c60e2fc90140e5007
NigerianPrinces <57740526+NigerianPrinces@users.noreply.github.com>
2019-11-15 13:35:30 -0800

    Create IIIIIIL.py

---
+++ IIIIIIL.py
@@ -0,0 +1,7 @@
+
+import subprocess as yeet
+
+
+x = Input("Which One Sire?")
+yeet.run(["mv /cyberpatwinsound /drive location"], shell = false)
+yeet.run(["cp /drive location/" + x + " /cyberpat/[win].wav" ], shell = false)
