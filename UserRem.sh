73b20d358fd7573e4423b11d2ac819810bdbc8b8
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 21:10:38 -0800

    Update UserRem.sh

--- UserRem.sh
+++ UserRem.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
-looop=1
-while "$looop"; do
-        read -p "User to be removed? userrem
-        user_exists=$(getent passwd $userrem)
-        if [[ -z "$user_exists" ]]
-        then
-                echo "User $userrem does not exist"
-        else
-                userdel -rf "$userrem"
-                echo "Omae wa mou shindeiru, motherfucker. I yeeted $userrem"
-        fi
-        read -r "Remove another user y/n" next
-        if [[ "$next" != "y" ]];
-        then
-                looop=0
-        fi
-done
+read -p "User to be removed? userrem
+user_exists=$(getent passwd $userrem)
+if [[ -z "$user_exists" ]]
+then
+       echo "User $userrem does not exist"
+else
+       userdel -rf "$userrem"
+       echo "Omae wa mou shindeiru, motherfucker. I yeeted $userrem"
+fi
