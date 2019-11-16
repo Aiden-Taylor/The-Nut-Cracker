d2244c9b2e7bdc6981bb25c2918ec0445f76d531
JacksonElsemore <54335566+JacksonElsemore@users.noreply.github.com>
2019-11-15 16:59:14 -0800

    Update Ports.py
    The other day I was walking in the park and a bee stung me and immediately died. I envied the bee. It could make its mark and then leave this plain of existence. I thought of my failure of contributing to this script. I cried.

--- Ports.py
+++ Ports.py
@@ -15,12 +15,12 @@ if kekel == "y" or kekel =="Y":
 		kekel = input("put in ports you want to block in format of     10,23,22,80... its pretty sad i need to explicitly state this, really goes to show the amount of brain cells you have")
 		things = kekel.split(",")
 	for port in things: 
-		yeet.run(["ufw deny",port],shell=False)
+		yeet.run(["ufw deny " +port],shell=True)
 		print("Port #" + port + " has been blocked :)")
-		yeet.run(['echo "port #' + port + ' is done heker" >> log.txt'],shell=False)
+		yeet.run(['echo "port #' + port + ' is done heker" >> log.txt'],shell=True)
 	kekel = input("Do you want to save changes across reboot? (Y/N)")
 	if kekel == "Y":
-		yeet.run(["service iptables save"],shell=False)
+		yeet.run(["service iptables save"],shell=True)
 	yeet.run(['echo "ports are heked you heker" >> log.txt'],shell=False)
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #                                        MOVING ON TO SERVICES
@@ -37,10 +37,10 @@ if kekel == "y" or kekel == "Y":
 		print("PS ur mom is a ho")
 		things = kekel.split(",")
 	for service in things:
-		yeet.run(["systemctl stop",service],shell=False)
-		yeet.run(["systemctl disable",service],shell=False)
-		yeet.run(["systemctl daemon-reload"],shell=False)
-		yeet.run(["systemctl reset-failed"],shell=False)
+		yeet.run(["systemctl stop " +service],shell=True)
+		yeet.run(["systemctl disable " +service],shell=True)
+		yeet.run(["systemctl daemon-reload"],shell=True)
+		yeet.run(["systemctl reset-failed"],shell=True)
 		print("Service " + service + " has been blocked :)")
 		yeet.run(['echo "Service '+service+' is blocked" >> log.txt'],shell=False)
 	yeet.run(['echo "Services done just like ur mom" >> log.txt'],shell=False)
