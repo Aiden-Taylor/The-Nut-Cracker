ae9bac2042e53aab56c2f0d9d6c93e241d2f7711
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 20:21:50 -0800

    Update UserRem.sh
    added loop on bash level

--- UserRem.sh
+++ UserRem.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-read -p "User to be removed? userrem
-user_exists=$(getent passwd $userrem)
-if [ -z "$user_exists" ] 
-then
-        echo "User $userrem does not exist"
-else
-        userdel -rf "$userrem"
-        echo "Omae wa mou shindeiru, motherfucker. I yeeted $userrem"
-fi
+looop=1
+while "$looop"; do
+        read -p "User to be removed? userrem
+        user_exists=$(getent passwd $userrem)
+        if [ -z "$user_exists" ] 
+        then
+                echo "User $userrem does not exist"
+        else
+                userdel -rf "$userrem"
+                echo "Omae wa mou shindeiru, motherfucker. I yeeted $userrem"
+        fi
+        read -r "Remove another user? (y/n)" next
+        if [[ "$next" != "Y" ]] && [[ "$next != "y" ]]
+        then
+                looop=0
+        fi
+done
