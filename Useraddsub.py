52d918425fe8f20b46a78c3c0e976ec6be39b308
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 19:12:02 -0800

    Update Useraddsub.py
    added usability

--- Useraddsub.py
+++ Useraddsub.py
@@ -3,3 +3,6 @@ if users == "y" or users == "Y":
     bump = 0
 while bump == 0:
     yeet.Popen(["./UserAdd.sh",], shell=True)
+    users = input("Does a user need to be added, poopyhead? (y/n)")
+    if users == "n" or users == "N":
+        bump = 0
