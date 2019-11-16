7ec3308989c40aca6aca1238cefab4e477776974
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 19:50:30 -0800

    Update Userremsub.py

--- Userremsub.py
+++ Userremsub.py
@@ -1,1 +1,13 @@
 import subprocess as yeet
+yeet.Popen(["chmod +x UserRem.sh"], shell=True)
+import time as bruhmoment
+users = input("Does a user need to be removed, fart sniffer? (y/n)")
+if users == "y" or users == "Y":
+    bump = 0
+while bump == 0:
+    x = input("Name of the user, idiot?")
+    yeet.Popen(["./UserRem.sh"], shell=True)
+    bruhmoment.sleep(1)
+    users = input("Does another user need to be removed, booger connoisseur? (y/n)")
+    if users != "y" and users != "Y":
+        bump = 1
