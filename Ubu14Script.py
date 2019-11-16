d0bfb7178da1451a5fe9866fc7d391a3e1efe9ef
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 20:05:16 -0800

    Update Ubu14Script.py

--- Ubu14Script.py
+++ Ubu14Script.py
@@ -109,9 +109,25 @@ if kekel == "y" or kekel == "Y":
 #                                        MOVING ON TO USERS
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #user removal
-yeet.Popen(["python3 ./Userrem.sub"], shell=True)
+users = input("Does a user need to be removed, fart sniffer? (y/n)")
+if users == "y" or users == "Y":
+    bump = 0
+while bump == 0:
+    yeet.Popen(["python3 ./Userrem.sub"], shell=True)
+    godfuckingdamnit.sleep(1)
+    users = input("Does another user need to be removed, booger connoisseur? (y/n)")
+    if users != "y" and users != "Y":
+        bump = 1
 #user adding
-yeet.Popen(["python3 ./Useradd.sub"], shell=True)
+users = input("Does a user need to be added, poopyhead? (y/n)")
+if users == "y" or users == "Y":
+    bump = 0
+while bump == 0:
+    yeet.Popen(["python3 ./Useradd.sub"], shell=True)
+    godfuckingdamnit.sleep(1)
+    users = input("Does a user need to be added, poopyhead? (y/n)")
+    if users != ("Y" and "y"):
+        bump = 0
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
 #                                        MOVING ON TO PASSWORDS
 #---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---=---
