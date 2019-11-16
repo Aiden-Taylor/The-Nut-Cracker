403d68d3348c9874ba2485a362a3bf3b48757dd0
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 19:18:20 -0800

    Update Useraddsub.py
    added a bruhmoment to reduce bruh moments.

--- Useraddsub.py
+++ Useraddsub.py
@@ -1,9 +1,11 @@
 import subprocess as yeet
+import time as bruhmoment
 users = input("Does a user need to be added, poopyhead? (y/n)")
 if users == "y" or users == "Y":
     bump = 0
 while bump == 0:
     yeet.Popen(["./UserAdd.sh",], shell=True)
+    bruhmoment.sleep(1)
     users = input("Does a user need to be added, poopyhead? (y/n)")
     if users == "n" or users == "N":
         bump = 0
