a2b08ae1c77d290ec3408fae0c22faac24c51cba
JacksonElsemore <54335566+JacksonElsemore@users.noreply.github.com>
2019-11-15 17:00:25 -0800

    Update autoUpdatter.py
    started doing some more stuff

--- autoUpdatter.py
+++ autoUpdatter.py
@@ -2,6 +2,7 @@ import subprocess as yeet
 kekel = input("You wan to do the upating thing you knuckle headed bufoon")
 if kekel == "Y" or kekel == "y":
   yeet.run(["apt-get install unattended-upgrades"],shell=False)
-  yeet.run(["sed -i 's/fuck/Jackson/g' /etc/apt/apt.conf.d/10periodic"],shell=True) #change fuck and Jackson into what you want
+  yeet.run(["sed -i '16,19d' /etc/apt/apt.conf.d/50unattended"],shell=True)
+  yeet.run(["sed -i 's/[//  \"$]/   \"$/g' /etc/apt/apt.conf.d/50unattended"],shell=True)
+  yeet.ru(["sed -i 's///g' /etc/apt/apt.conf.d/10period"],shell=True)
  
-  
