79463e858329671292c658d172056770530a7591
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 20:47:31 -0800

    Update UserRem.sh

--- UserRem.sh
+++ UserRem.sh
@@ -3,7 +3,7 @@ looop=1
 while "$looop"; do
         read -p "User to be removed? userrem
         user_exists=$(getent passwd $userrem)
-        if [[ -z "$user_exists" ]];
+        if [[ -z "$user_exists" ]]
         then
                 echo "User $userrem does not exist"
         else
@@ -11,7 +11,7 @@ while "$looop"; do
                 echo "Omae wa mou shindeiru, motherfucker. I yeeted $userrem"
         fi
         read -r "Remove another user y/n" next
-        if [[ "$next" != "Y" ]] && [[ "$next != "y" ]];
+        if [[ "$next" != "Y" ]] && [[ "$next" != "y" ]];
         then
                 looop=0
         fi
