830775f6e72de56554ca41d4743661dabd4b7d20
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2019-11-15 19:50:20 -0800

    Create UserRem.sh

---
+++ UserRem.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+read -p "User to be removed? userrem
+user_exists=$(getent passwd $userrem)
+if [ -z "$user_exists" ] 
+then
+        echo "User $userrem does not exist"
+else
+        userdel -rf "$userrem"
+        echo "Omae wa mou shindeiru, motherfucker. I yeeted $userrem"
+fi
